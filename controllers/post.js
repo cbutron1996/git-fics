8e18ce77d65a240ded5fefb62da09dd22e8ae050
Christian Butron <cbutron1996@gmail.com>
2018-04-03 23:59:47 -0400

    Added story bodies

--- controllers/post.js
+++ controllers/post.js
@@ -4,6 +4,7 @@ const models = require('../models');
 const router = express.Router();
 const git = require('simple-git');
 const mkdirp = require('mkdirp');
+const fs = require('fs');
 
 router.get('/', (req, res) => {
   res.render('post');
@@ -11,7 +12,11 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   mkdirp('./stories/' + req.body.title); // makes new folder
-  git('stories/' + req.body.title).init(); // inits folder as new repo
+  fs.writeFile("./stories/" + req.body.title + "/chapter1.txt", req.body.story);
+  git('stories/' + req.body.title)
+  .init()
+  .add('./*')
+  .commit("first commit!");
   res.render('post');
 });
 
